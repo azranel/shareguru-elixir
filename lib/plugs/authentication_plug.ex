85b0ce9889e8c672b0132c2d325928db60dab0b1
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2016-07-08 22:56:37 +0200

    Add authentication plug

---
+++ lib/plugs/authentication_plug.ex
@@ -0,0 +1,19 @@
+defmodule Shareguru.AuthenticationPlug do
+  import Plug.Conn
+  import Plug.Session
+
+  def init(options), do: options
+
+  def call(conn, options) do
+    case get_session(conn, :current_user) do
+      nil -> do_redirect(conn, Shareguru.Router.Helpers.auth_path(Shareguru.Endpoint, :request, "google"))
+      _ -> conn
+    end
+  end
+
+  defp do_redirect(conn, to) do
+    conn
+      |> Phoenix.Controller.redirect(to: to)
+      |> halt
+  end
+end

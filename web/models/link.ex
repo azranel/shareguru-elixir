6a2c999990a2961e5a2d380f059791dc1cb8b5bd
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2016-07-08 17:51:13 +0200

    add migration and link model

---
+++ web/models/link.ex
@@ -0,0 +1,25 @@
+defmodule Shareguru.Link do
+  use Shareguru.Web, :model
+
+  schema "links" do
+    field :description, :string
+    field :url, :string
+
+    timestamps
+  end
+
+  @required_fields ~w(description url)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+    |> unique_constraint(:url)
+  end
+end

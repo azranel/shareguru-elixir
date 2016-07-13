ed9a444894f73f9f9f03e14ae1b16a639655031f
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2016-07-13 23:51:40 +0200

    Fix user specs

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,7 +3,7 @@ defmodule Shareguru.UserTest do
 
   alias Shareguru.User
 
-  @valid_attrs %{email: "cool_email@email.com", expire_at: "2010-04-17 14:00:00", name: "Bartosz Łęcki", picture: "http://google.pl/some_cool_picture.png"}
+  @valid_attrs %{email: "cool_email@netguru.pl", expire_at: "2010-04-17 14:00:00", name: "Bartosz Łęcki", picture: "http://google.pl/some_cool_picture.png"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do

12d3d51531b0e92d220ff505a31adcd087004d07
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2016-09-21 00:51:47 +0200

    simplify Authentication Plug
    let’s just assign user in conn, instead of using session - it’s easier
    to mock signed in user that way

--- lib/plugs/authentication_plug.ex
+++ lib/plugs/authentication_plug.ex
@@ -1,14 +1,17 @@
 defmodule Shareguru.AuthenticationPlug do
   import Plug.Conn
   import Plug.Session
+  alias Shareguru.Router.Helpers
+  alias Shareguru.Endpoint
 
   def init(options), do: options
 
+  def call(%{assigns: %{current_user: user}} = conn, _) when user != nil do
+    conn
+  end
+
   def call(conn, _) do
-    case get_session(conn, :current_user) do
-      nil -> do_redirect(conn, Shareguru.Router.Helpers.auth_path(Shareguru.Endpoint, :request, "google"))
-      _ -> conn
-    end
+    do_redirect(conn, Helpers.auth_path(Endpoint, :request, "google"))
   end
 
   defp do_redirect(conn, to) do

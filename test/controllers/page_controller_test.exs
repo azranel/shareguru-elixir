c04d057ad0b491347fa3cfdda85f7d0f45957abe
azranel <chunkyo@gmail.com>
2016-09-22 22:26:33 +0300

    Fix tests for controllers

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,29 +1,22 @@
 defmodule Shareguru.PageControllerTest do
   use Shareguru.ConnCase
 
-  alias Plug.Conn
-
-  @default_opts [
-    store: :cookie,
-    key: "foobar",
-    encryption_salt: "encrypted cookie salt",
-    signing_salt: "signing salt",
-    log: false
-  ]
-  @signing_opts Plug.Session.init(Keyword.put(@default_opts, :encrypt, false))
-  @secret_key String.duplicate("abcdef0123456789", 8)
-
   test "GET / when not logged in", %{conn: conn} do
     conn = get conn, "/"
+    assert Shareguru.UserSession.current_user(conn) == nil
     assert redirected_to(conn) =~ "/auth/google"
   end
 
   test "GET / when logged in", %{conn: conn} do
-    conn = put_in(conn.secret_key_base, @secret_key)
-      |> Plug.Session.call(@signing_opts)
-      |> fetch_session
-      |> Conn.put_session(:current_user, "Sample user")
-      |> get("/")
-    assert redirected_to(conn) =~ "/auth/google"
+    user = %Shareguru.User{
+      id: 123,
+      name: "dude",
+      email: "dude@example.com"
+    }
+
+    conn = conn 
+    |> with_current_user(user) 
+    |> get("/")
+    assert conn.status == 200
   end
 end
